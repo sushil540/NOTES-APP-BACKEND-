0494cc9b97c45c25abaf40ee2b3bddfc87b5b86c
Sushilendra G P <77796514+sushil540@users.noreply.github.com>
2026-03-21 14:23:57 +0530

    Update MongoDB connection string in configDB.js

--- configureDB/configDB.js
+++ configureDB/configDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const configureDB = async () => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/notes-app")
+        await mongoose.connect("mongodb://mongo:GVwzcoQYABscoHBsFQCXsuPBtHhOKAAd@caboose.proxy.rlwy.net:55004")//"mongodb://127.0.0.1:27017/notes-app"
         console.log("Connected to db")
     }catch(error){
         console.log("Error connecting to db", error)
